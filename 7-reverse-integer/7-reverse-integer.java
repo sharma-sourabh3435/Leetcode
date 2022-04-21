292ecdf9f8f9975b12ae568509d0695226816627
Sourabh Sharma <62477502+sharma-sourabh3435@users.noreply.github.com>
2022-04-21 17:01:35 -0300

    Time: 2 ms (63.85%), Space: 41 MB (67.49%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int reverse(int x) {
+        int num = 0;
+        while(x != 0) {
+            int number = num*10 + x%10;
+
+            if((number - x%10)/10 != num) {
+                return 0;
+            }
+            else {
+                num = number;
+            }
+            x = x / 10;
+        }
+        return num;
+    }
+}

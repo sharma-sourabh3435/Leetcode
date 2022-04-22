194a8f18b18de99db929fecb89ec18cdc6fc4912
Sourabh Sharma <62477502+sharma-sourabh3435@users.noreply.github.com>
2022-04-21 23:07:58 -0300

    Time: 5 ms (59.03%), Space: 45.7 MB (35.45%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -2,8 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         // intialize a new hashmap
         HashMap<Integer, Integer> meow = new HashMap<Integer, Integer>();
-        // if the hashmap contains value equal to complement then return index i and index of 
-        // complement else put the index and value inside the hashmap
+        
         for(int i = 0; i < nums.length; i++) {
             int complement = target - nums[i]; 
             if(meow.containsKey(complement)) {

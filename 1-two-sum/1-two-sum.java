d12b42cdb3e1fcb84788f8432688854be3d434b3
Sourabh Sharma <62477502+sharma-sourabh3435@users.noreply.github.com>
2022-04-21 21:18:44 -0300

    Time: 1 ms (99.70%), Space: 42.1 MB (97.49%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> meow = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i]; 
+            if(meow.containsKey(complement)) {
+                return new int[] {i, meow.get(complement)};
+            }
+            else {
+                meow.put(nums[i], i);
+            }
+        }
+        return new int[] {};
+    }
+}
